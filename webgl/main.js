3386f7eed8d0187f362ec6601f5ac2cfc4a578a3
madebyrag <madebyrag@qq.com>
2015-07-10 21:18:39 +0800

    完整版讲义

--- webgl/main.js
+++ webgl/main.js
@@ -1,15 +1,24 @@
 function setupBuffers() {
     vertexBuffer = gl.createBuffer();
     gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-    var triangleVertices = [
-         0.0,  0.5,  0.0,
+    var lineVertices = [
+        -0.8, -0.2,  0.0,
+        -0.8, -0.5,  0.0,
+        -0.8, -0.5,  0.0,
+        -0.7, -0.5,  0.0,
+        -0.6, -0.5,  0.0,
         -0.5, -0.5,  0.0,
-         0.5, -0.5,  0.0,
-         1.0, -1.0,  0.0
+        -0.5, -0.5,  0.0,
+        -0.5, -0.4,  0.0,
+        -0.5, -0.4,  0.0,
+        -0.6, -0.4,  0.0,
+        -0.6, -0.4,  0.0,
+        -0.6, -0.5,  0.0,
+         
     ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(lineVertices), gl.STATIC_DRAW);
     vertexBuffer.itemSize = 3;
-    vertexBuffer.numberOfItems = 4;
+    vertexBuffer.numberOfItems = 12;
 }
 
 function draw() {
@@ -19,7 +28,7 @@ function draw() {
     gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, vertexBuffer.itemSize, 
                             gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(shaderProgram.vertexPositionAttribute);
-    gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertexBuffer.numberOfItems);
+    gl.drawArrays(gl.LINES, 0, vertexBuffer.numberOfItems);
 }
 
 function startup() {
