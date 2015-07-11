32a847c47b05d4b059d334f95a88e9715730db80
madebyrag <madebyrag@qq.com>
2015-07-12 00:42:33 +0800

    新增输入框提示

--- front/css/index.js
+++ front/css/index.js
@@ -1,6 +1,9 @@
 var menu_btn = document.getElementById('menu_btn');
 var drop_down = document.getElementById('drop_down');
 drop_down.status = 'up';
+var group = ['ALG','Web','Android','PM','Design','IT','ESD','IOS'];
+
+var drop_container = document.getElementById('drop_container');
 
 menu_btn.addEventListener('click',function(){
     if (drop_down.status == 'up'){
@@ -17,6 +20,7 @@ menu_btn.addEventListener('click',function(){
 });
 
 var body = document.getElementsByTagName('body')[0];
+
 window.onresize = function(){ 
     if (body.offsetWidth >= 810){
         console.log(1);
@@ -25,3 +29,24 @@ window.onresize = function(){
         drop_down.status = 'up';
     }
 };
+
+//regexp    search
+var search = document.getElementById('search');
+search.addEventListener('input',function(){
+    drop_container.innerHTML = '';
+    var re = new RegExp(this.value, 'i');
+    for (var i in group) {
+        if (group[i].match(re) && this.value != '') {
+            var new_ele = document.createElement('a');
+            new_ele.className = 'search_drop_list';
+            new_ele.innerHTML = group[i];
+            new_ele.href = 'javascript:void(0)';
+            new_ele.addEventListener('click',function(){
+                search.value = this.innerHTML;
+                drop_container.innerHTML = '';
+            });
+            drop_container.appendChild(new_ele);
+        }
+    }
+    delete re;
+});
